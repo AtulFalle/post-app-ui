88909d5c966ebc6771dd3fd8eb25b3ea2bcccc38
AtulFAlle <atulfalle007@gmail.com>
2025-03-26 00:42:29 +0530

    added auth guard to dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,15 @@ import { AuthGuard } from './core/guard/auth.guard';
 export const routes: Routes = [
     {
         path: 'auth',
-        loadChildren: () => import('./auth/auth.module').then(m=> m.AuthModule)
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
     },
     {
         path: 'dashboard',
-        loadComponent: () => import('./components/post-list/post-list.component').then( m => m.PostListComponent),
-        canActivate: [AuthGuard] 
+        loadComponent: () => import('./components/post-list/post-list.component').then(m => m.PostListComponent),
+        canActivate: [AuthGuard]
     },
     {
-        path:'post-details/:id',
+        path: 'post-details/:id',
         loadComponent: () => import('./components/post-detail/post-detail.component').then(m => m.PostDetailComponent)
     },
     {
